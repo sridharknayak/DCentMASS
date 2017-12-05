b2e2fac2e36a1b80f117b2c789f1ef47e7517513
Tejas <tej.sourceus@gmail.com>
2017-12-05 12:50:19 +0530

    Basic content + structure

---
+++ js/script.js
@@ -0,0 +1,3 @@
+function newFunction(params) {
+    //Body
+}
---
+++ signup.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Author name: <Your name>;
+E-mail ID:   <Your E-mail ID>
+Date:        <dd-mm-yyyy>
+*/
+/////////////////////////////////////////////////////////
+
+//include_once("variables.php");
+//include_once("checkLoginStatus.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Title</title>
+    <?php include_once("stylesAndFont.php"); ?>
+</head>
+<body>
+<?php include_once("header.php"); ?>
+<!-- BODY START -->
+    
+<!-- BODY END -->
+<?php include_once("footer.php"); ?>
+<?php include_once("script.php"); ?>
+</body>
+</html>
