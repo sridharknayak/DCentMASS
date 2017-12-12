a0088d0d62907303a9709fc6dbc05b10646f2278
Tejas <33614803+Internet-Unexplorer@users.noreply.github.com>
2017-12-12 16:34:01 +0530

    Update stylesAndFonts.php

--- stylesAndFonts.php
+++ stylesAndFonts.php
@@ -1,2 +1,6 @@
+<!-- Bootstrap CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom CSS -->
 <link rel="stylesheet" type="text/css" href="css/style.css">
+<!-- SASS output -->
 <link rel="stylesheet" type="text/css" href="css/style1.css">
