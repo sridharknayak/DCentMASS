f26543fc43023b2701c788ebfc94ef824b091825
Tejas <33614803+Internet-Unexplorer@users.noreply.github.com>
2017-12-12 16:21:13 +0530

    Updated.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,49 @@
 /*
-Author name: <Your name>;
-E-mail ID:   <Your E-mail ID>
-Date:        <dd-mm-yyyy>
+Author name: Tejas;
+E-mail ID:   tej.sourceus@gmail.com
+Date:        12-12-2017
 */
 
-function newFunction(params) {
-    //Body
+// Read window width on load
+    	var wdth = $(window).width();
+    	$(document).ready(function(){
+		    if(wdth<"880")
+				{
+				   	$(".right-bars").css("display","none");
+				   	$(".left-bar").css("display","none");
+				   	$("#openNavID").css("display","inline-block");
+				}
+				else if(wdth>="880")
+				{
+				   	$(".right-bars").css("display","block");
+				   	$(".left-bar").css("display","block");
+				   	$("#openNavID").css("display","none");
+				}
+		});
+		// Read window width change
+		$(window).on('resize', function(){
+		   if($(this).width() != wdth){
+		      wdth = $(this).width();
+		       if(wdth<"880")
+				{
+				   	$(".right-bars").css("display","none");
+				   	$(".left-bar").css("display","none");
+				   	$("#openNavID").css("display","inline-block");
+				}
+				else if(wdth>="880")
+				{
+				   	$(".right-bars").css("display","block");
+				   	$(".left-bar").css("display","block");
+				   	$("#openNavID").css("display","none");
+				}
+		   }
+		});
+
+		
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
