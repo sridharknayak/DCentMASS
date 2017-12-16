f8e70febed48c6baad695f19ffaeb93e441b391d
Nadeem <nadeem566sourceus@gmail.com>
2017-12-16 11:26:13 +0530

    interview skills

--- MustKnow.php
+++ MustKnow.php
@@ -98,11 +98,11 @@ Date:        15/12/17
 <div class="row">
 <div class="col-md-3">
 <ul>
-<button class="btn btn-success btnpad" type="submit">Interview Skills</button><br />
-<button class="btn btn-success btnpad" type="submit">Aptitude</button><br />
-<button class="btn btn-success btnpad" type="submit">Technical Education</button><br />
-<button class="btn btn-success btnpad" type="submit">Resume Preperation</button><br />
-<button class="btn btn-success btnpad" type="submit">Soft Skills</button><br />
+<a href="#section1"><button class="btn btn-success btnpad" type="submit">Interview Skills</button></a><br />
+<a href="#section2"><button class="btn btn-success btnpad" type="submit">Aptitude</button></a><br />
+<a href="#section3"><button class="btn btn-success btnpad" type="submit">Technical Education</button></a><br />
+<a href="#section4"><button class="btn btn-success btnpad" type="submit">Resume Preperation</button></a><br />
+<a href="#section5"><button class="btn btn-success btnpad" type="submit">Soft Skills</button></a><br />
 </ul>
 </div>
 <div class="col-md-9">
@@ -117,9 +117,66 @@ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut sint laborum maior
 
 <div class="row">
   <div class="col-md-12">
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, exercitationem! Quibusdam vel consequatur incidunt reiciendis aspernatur dolorum atque dicta quas? Quia, accusamus laudantium cumque laborum quisquam asperiores ex suscipit magni?
+  <div id="section1" class="container-fluid">
+  <h1>Interview Skills</h1>
+  <h3> 10 Tips </h3>
+  <h5>1) Practice good nonverbal communication</h5>
+<p>It's about demonstrating confidence: standing straight, making eye contact and connecting with a firm handshake. That first nonverbal impression can be a great beginning—or quick ending—to your interview.
 </p>
+<h5>2) Dress for the job or company</h5>
+<p>Today's casual dress codes do not give you permission to dress as "they" do when you interview. It is important to know what to wear to an interview and to be well-groomed. Whether you wear a suit or something less formal depends on the company culture and the position you are seeking. If possible, call to find out about the company dress code before the interview.
+</p>
+<h5>3) Listen</h5>
+<p>From the very beginning of the interview, your interviewer is giving you information, either directly or indirectly. If you are not hearing it, you are missing a major opportunity. Good communication skills include listening and letting the person know you heard what was said. Observe your interviewer, and match that style and pace.
+</p>
+<h5>4) Don't talk too much</h5>
+<p>Telling the interviewer more than he needs to know could be a fatal mistake. When you have not prepared ahead of time, you may ramble when answering interview questions, sometimes talking yourself right out of the job. Prepare for the interview by reading through the job posting, matching your skills with the position's requirements and relating only that information.
+</p>
+<h5>5) Don't be too familiar</h5>
+<p>The interview is a professional meeting to talk business. This is not about making a new friend. Your level of familiarity should mimic the interviewer's demeanor. It is important to bring energy and enthusiasm to the interview and to ask questions, but do not overstep your place as a candidate looking for a job.
+</p>
+<h5>6) Use appropriate language</h5>
+<p>It's a given that you should use professional language during the interview. Be aware of any inappropriate slang words or references to age, race, religion, politics or sexual orientation—these topics could send you out the door very quickly.
+</p>
+<h5>7) Don't be cocky</h5>
+<p>Attitude plays a key role in your interview success. There is a fine balance between confidence, professionalism and modesty. Even if you're putting on a performance to demonstrate your ability, overconfidence is as bad, if not worse, as being too reserved.
+</p>
+<h5>8) Take care to answer the questions</h5>
+<p>When interviewers ask for an example of a time when you did something, they are asking behavioral interview questions, which are designed to elicit a sample of your past behavior. If you fail to relate a specific example, you not only don't answer the question, but you also miss an opportunity to prove your ability and talk about your skills.
+</p>
+<h5>9) Ask questions</h5>
+<p>When asked if they have any questions, most candidates answer, "No." Wrong answer. Part of knowing how to interview is being ready to ask questions that demonstrate an interest in what goes on in the company. Asking questions also gives you the opportunity to find out if this is the right place for you. The best questions come from listening to what you're asked during the interview and asking for additional information.
+</p>
+<h5>10) Don't appear desperate</h5>
+<p>When you interview with the "please, please hire me" approach, you appear desperate and less confident. Reflect the three Cs during the interview: cool, calm and confidence. You know you can do the job; make sure the interviewer believes you can, too.
+</p>
+<a href="#" class="btnpad1"><button class="btn btn-success" type="submit">UP</button></a>
+</div>
+<div id="section2" class="container-fluid">
+  <h1>Aptitude</h1>
+  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+  <a href="#" class="btnpad1"><button class="btn btn-success" type="submit">UP</button></a>
+  </div>
+  
+<div id="section3" class="container-fluid">
+  <h1>Technical Education</h1>
+  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+  <a href="#" class="btnpad1"><button class="btn btn-success" type="submit">UP</button></a>
+</div>
+<div id="section4" class="container-fluid">
+  <h1>Resume Preperation</h1>
+  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+  <a href="#" class="btnpad1"><button class="btn btn-success" type="submit">UP</button></a>
+</div>
+<div id="section5" class="container-fluid">
+  <h1>Soft Skills</h1>
+  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+  <a href="#" class="btnpad1"><button class="btn btn-success" type="submit">UP</button></a>
+</div>
   </div>
 </div>
 
