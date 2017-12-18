46bcde8662a138b40dade828ab1a96585b90e78e
Nadeem <nadeem566sourceus@gmail.com>
2017-12-18 12:50:17 +0530

    added resource consulting

--- ResourceConsulting.php
+++ ResourceConsulting.php
@@ -16,7 +16,7 @@ Date:        16/12/17
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Title</title>
+    <title>Resource Consulting</title>
     <?php include_once("stylesAndFont.php"); ?>
 </head>
 <body>
@@ -29,7 +29,7 @@ Date:        16/12/17
         <!-- User class="activeLink" to make links active -->
         <li><a href="#">Home</a></li>
         <li><a href="#">DCent MASS</a></li>
-        <li><a href="#">Our Services</a></li>
+        <li class="activeLink"><a href="#">Our Services</a></li>
         <li><a href="#">Student Hub</a></li>
         <li><a href="#">Courses</a></li>
         <li><a href="#">Events</a></li>
@@ -62,25 +62,25 @@ Date:        16/12/17
      <!-- Right bar starts -->
      <div class="right-bars">
      	<div class="righ-col-1"> <!-- User class="activeLink" to make links active -->
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 1</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 2</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 3</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 4</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 5</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 6</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 7</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 8</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Classroom Training</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Online Training</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Corporate Training</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">College Internship</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Job Placements</span></div></a>
+        <a href="#"><div class="rightMenuBlocks activeLink"><span class="linkText">Resource Consulting</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Interview Services</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Our Recommendation</span></div></a>
          
       </div>
      	<div class="righ-col-2"> <!-- User class="activeLink" to make links active -->
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 9</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 10</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 11</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 12</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 13</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 14</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 15</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 16</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Job Seekers</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Emplpoyer</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Partner Services</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText"></span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText"></span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText"></span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText"></span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText"></span></div></a>
      	</div>
      </div>
      
@@ -94,13 +94,60 @@ Date:        16/12/17
 
 
 
-
+<div class="col=md-12">
 <!-- WRITE MAIN CONTENT HERE -->
+
+<h1>Resource Consulting</h1><hr>
+<p>RESOURCE CONSULTING is a Recruitment and HR Consulting company, offering job search services and strategic people solutions to Multinational Companies and Small-and-Medium Sized Enterprises in Singapore and the Asia Pacific region. Our team of experienced and dedicated professionals are committed to provide prompt and reliable services to our clients and candidates. We believe that our acute ability to understand our clients' needs and comprehensive approach to talent acquisition, assessment, management and development, have enabled us to support our clients in making people their sustainable source of competitive advantag
+</p>
+<p>
+The following are core fields around which most human resource consultancies are based:
+
+<ul>Employee engagement: measure employee engagement levels through surveys and interviews, define and improve performance in employee engagement and retention. While this area of HR consulting is necessarily broad, encapsulating total rewards strategy, employee performance management, leadership transformation, and organisation structure design,[2] most companies do have very specialised independent practices.[3][4]
+</ul>
+<ul>Compensation: design and manage compensation programs related to basic salary, bonuses, and stock plans. Evaluation of positions and building of salary structures, bonus plans and stock plans for clients are common.[5] Specialisations are often based on employee types (e.g. Executive compensation consultants[6] and sales compensation consultants[7]).
+</ul>
+<ul>Employee benefits: optimize benefit plan design and administration (inclusive of health-related benefits) by assessing competitiveness and effectiveness of benefit plans (analytics and design), and cost-effectiveness and quality of vendors (brokerage).[8][9]
+</ul>
+<ul>Actuarial and retirement: provide actuarial and administration services to manage cost and effectiveness of retirement programs, including defined benefit and defined contribution plans.[10]
+</ul>
+<ul>Mergers and acquisitions: conduct human capital due diligence, coordinate and administer cross-functional activities during execution, including payroll and HR technology. Align organizational cultures and work styles during post-merger integration.[11][12][13]
+</ul>
+<ul>Talent mobility: Provides the insight and execution for full international expatriates (usually for executives) or local plus (partial-package expatriates), from pre-move informative guide, to post-move expat management program.[14]
+
+Other services may also include legal counseling, global initiatives, investments consulting, and the implementation of human resource technologies to facilitate human capital management.
+</p>
+
+<div class="jumbotron nadj1">
+<center><h2>Keep up with Resource Consulting Pte Ltd</h2></center><br/>
+<div class="row">
+<div class="col-md-4">
+<p>
+Stay up to date with company news</p>
+</div>
+<div class="col-md-4">
+<p>
+Discover new job opportunities</p>
+</div>
+<div class="col-md-4">
+<p>See how you're connected to employees</p>
+</div>
+</div>
+</div>
+
+<div class="jumbotron nadj2">
+ <img src="images/i6.png" alt="" class="nadi6 img-responsive">
+ <p>
+ Resource Consulting is a Recruitment and HR Consulting company, offering job search services and strategic people solutions to Multinational Companies and Small-and-Medium Sized Enterprises in Singapore and the Asia Pacific region.
+</p><p>
+Our team of experienced and dedicated professionals are committed to provide prompt and reliable services to our clients and candidates. We believe that our acute ability to understand our clients' needs and comprehensive approach to talent acquisition, assessment, management and development, have enabled us to support our clients in making people their sustainable source of competitive advantage.
+ </p>
+</div>
 
 
 
 <!-- NOTE: Please keep your code clean + readable and add comments -->
-
+</div>
 
 
 
