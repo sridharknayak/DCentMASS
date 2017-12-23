7d9ffa2dd24f5b4e8387e6763ef106086d1e09ca
MdAsagar <asagardesaisourceus@gmail.com>
2017-12-23 06:13:55 -0800

    edited

--- corporateTraining.php
+++ corporateTraining.php
@@ -19,24 +19,25 @@ Date:        <dd-mm-yyyy>
     <title>Corporate training</title>
     <?php include_once("stylesAndFont.php"); ?>
 </head>
-<body id="back-img">
+<body id="ad-back-img">
   	<!-- Left bar starts -->
      <div class="left-bar">
       <div class="image1">
-        <img src="images/dcentmass_png.png" class="img-responsive">
+        <img src="images/dcentmass_png.png" class="img-fluid">
       </div>
       <ul>
         <!-- User class="activeLink" to make links active -->
-        <li><a href="#">Home</a></li>
-        <li><a href="#">DCent MASS</a></li>
-        <li><a href="#">Our Services</a></li>
-        <li><a href="#">Student Hub</a></li>
-        <li><a href="#">Courses</a></li>
-        <li><a href="#">Events</a></li>
-        <li><a href="#">Media</a></li>
-        <li><a href="#">Forum</a></li>
-        <li><a href="#">Contact Us</a></li>
-        <li style="border-bottom:  2px solid #06f;"><a href="#">FAQs</a></li>
+        <li><a href="#"><i class="material-icons">home</i><span class="menuText">Home</span></a></li>
+        <li><a href="#"><i class="material-icons">language</i><span class="menuText">DCent MASS</span></a></li>
+        <li class="activeLink"><a href="#"><i class="material-icons">settings_applications</i><span class="menuText">Our Services</span></a></li>
+        <li><a href="#"><i class="material-icons">group_work</i><span class="menuText">Student Hub</span></a></li>
+        <li><a href="#"><i class="material-icons">library_books</i><span class="menuText">Courses</span></a></li>
+        <li><a href="#"><i class="material-icons">event</i><span class="menuText">Events</span></a></li>
+        <li><a href="#"><i class="material-icons">perm_media</i><span class="menuText">Media</span></a></li>
+        <!-- <li><a href="#">Forum</a></li> -->
+        <li><a href="#"><i class="material-icons">contacts</i><span class="menuText">Contact Us</span></a></li>
+        <!-- <li style="border-bottom:  2px solid #06f;"><a href="#"></a></li> -->
+        <!-- <li style="border-bottom: 2px solid #06f;"><a href="#">FAQs</a></li> -->
       </ul>
      </div>
      <!-- Left bar ends -->
@@ -65,7 +66,7 @@ Date:        <dd-mm-yyyy>
      	<div class="righ-col-1"> <!-- User class="activeLink" to make links active -->
         <a href="#"><div class="rightMenuBlocks"><span class="linkText">Classroom Training</span></div></a>
         <a href="#"><div class="rightMenuBlocks"><span class="linkText">Online Training</span></div></a>
-        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Corporate Training</span></div></a>
+        <a href="#"><div class="rightMenuBlocks activeLink"><span class="linkText">Corporate Training</span></div></a>
         <a href="#"><div class="rightMenuBlocks"><span class="linkText">College Internship</span></div></a>
         <a href="#"><div class="rightMenuBlocks"><span class="linkText">Job Placement</span></div></a>
         <a href="#"><div class="rightMenuBlocks"><span class="linkText">Resource Cosulting</span></div></a>
@@ -97,31 +98,31 @@ Date:        <dd-mm-yyyy>
 
 
 <!-- WRITE MAIN CONTENT HERE -->
-
+<div class="container-fluid">
 <div class="row">
- <div class="col-md-9 " >
+ <div class="col-md-9">
   
-          <h1 id="h-font">Corporate training</h1>
-          <p id="icon-Paragraph">Staying updated is the key to thrive in this competitive age. Our training solutions are tangible and make your teams proactive in the work plan and its execution. The highly professional team of Multisoft has successfully involved and drives success in a variety of different corporate cultures. We seamlessly determine your training requirements, mode of delivery and infrastructure required.</p>
+          <h1 id="ad-h-font">Corporate training</h1>
+          <p id="ad-main-Para">Staying updated is the key to thrive in this competitive age. Our training solutions are tangible and make your teams proactive in the work plan and its execution. The highly professional team of Multisoft has successfully involved and drives success in a variety of different corporate cultures. We seamlessly determine your training requirements, mode of delivery and infrastructure required.</p>
 
   <!--1st row-->
   <div class="container-fluid">
    <div class="row">
     <div class="col-md-4 text-center">
-            <img id="icon-img" class="img-circle" src="http://localhost/dcent/DCentMASS/images/img.png"  width="105px" height="105px">
+            <img id="ad-icon-img" class="img-circle" src="http://localhost/dcent/DCentMASS/images/img.png"  width="105px" height="105px">
         
-                 <h4 id="P-heading">Customized Corporate Training</h4>
-                <p id="icon-Paragraph">All the training details can be customized covering the duration, schedule, course material and most importantly an expert trainer </p><br>
+                 <h4 id="ad-P-heading">Customized Corporate Training</h4>
+                <p id="ad-icon-Paragraph">All the training details can be customized covering the duration, schedule, course material and most importantly an expert trainer </p><br>
 
     </div>
 
    <!--below icons-->
  
    <div class="col-md-4 text-center">
-      <img id="icon-img" class="img-circle" src="http://localhost/dcent/DCentMASS/images/13.png"  width="105px" height="105px">
+      <img id="ad-icon-img" class="img-circle" src="http://localhost/dcent/DCentMASS/images/13.png"  width="105px" height="105px">
  
-     <h4 id="P-heading">On Demand Training</h4>
-      <p id="icon-Paragraph">All the training details can be customized covering the duration, schedule, course material and most importantly an expert trainer </p>
+     <h4 id="ad-P-heading">On Demand Training</h4>
+      <p id="ad-icon-Paragraph">All the training details can be customized covering the duration, schedule, course material and most importantly an expert trainer </p>
 
    </div>
 
@@ -131,9 +132,9 @@ Date:        <dd-mm-yyyy>
 
        <div class="col-md-4 text-center">
       
-            <img id="icon-img" src="http://localhost/dcent/DCentMASS/images/b.png" class="img-circle" width="105px" height="105px">
-            <h4 id="P-heading">One-on-One Training</h4>
-            <p id="icon-Paragraph">Multisoft system One-on-One Training system permits 100% customization of training program and plan for individual aspirants. Key benefits are:</p>
+            <img id="ad-icon-img" src="http://localhost/dcent/DCentMASS/images/b.png" class="img-circle" width="105px" height="105px">
+            <h4 id="ad-P-heading">One-on-One Training</h4>
+            <p id="ad-icon-Paragraph">Multisoft system One-on-One Training system permits 100% customization of training program and plan for individual aspirants. Key benefits are:</p>
 
 
       </div>
@@ -145,64 +146,68 @@ Date:        <dd-mm-yyyy>
 <div class="row">
 
     <div class="col-md-4 text-center ">
-           <img id="icon-img" src="http://localhost/dcent/DCentMASS/images/c.png" class="img-circle" width="105px" height="105px">
-             <h4 id="P-heading">Fly Me A Trainer</h4>
-            <p id="icon-Paragraph">On Demand Training are provided by the skilled consultants from the relevant industries, focusing on the needs of the market. Training program provides:</p>
+           <img id="ad-icon-img" src="http://localhost/dcent/DCentMASS/images/c.png" class="img-circle" width="105px" height="105px">
+             <h4 id="ad-P-heading">Fly Me A Trainer</h4>
+            <p id="ad-icon-Paragraph">On Demand Training are provided by the skilled consultants from the relevant industries, focusing on the needs of the market. Training program provides:</p>
     </div>
 
 
   
     <div class="col-md-4  text-center">   
-         <img id="icon-img" src="http://localhost/dcent/DCentMASS/images/aaa.png"class="img-circle" width="105px" height="105px">
-         <h4 id="P-heading">Certification Based Corporate Training</h4>
-         <p id="icon-Paragraph">All the training details can be customized covering the duration, schedule, course material and most importantly an expert trainer</p>
+         <img id="ad-icon-img" src="http://localhost/dcent/DCentMASS/images/aaa.png"class="img-circle" width="105px" height="105px">
+         <h4 id="ad-P-heading">Certification Based Corporate Training</h4>
+         <p id="ad-icon-Paragraph">All the training details can be customized covering the duration, schedule, course material and most importantly an expert trainer</p>
         
    </div> 
 
 
 
   <div class="col-md-4 text-center">
-      <img id="icon-img" src="http://localhost/dcent/DCentMASS/images/bb.png" class="img-circle" width="105px" height="105px">
-      <h4 id="P-heading">Anytime, Anywhere – Across The Globe</h4>
-      <p id="icon-Paragraph">All the training details can be customized covering the duration, schedule, course material and most importantly an expert trainer</p>
+      <img id="ad-icon-img" src="http://localhost/dcent/DCentMASS/images/bb.png" class="img-circle" width="105px" height="105px">
+      <h4 id="ad-P-heading">Anytime, Anywhere – Across The Globe</h4>
+      <p id="ad-icon-Paragraph">All the training details can be customized covering the duration, schedule, course material and most importantly an expert trainer</p>
   </div>
 
 </div>
+
 </div>
 <br>
 
 
     <!--button-->
     
-   <center> <a href="#"  button  id="button-ex" type="button" class="btn btn-default btn-lg">Download Corporate Brochure</button></a></center>
-<!--
-  <div>
-        <button class="btn-3 btn-ex" ;">Button 3</button>
-  </div>-->
+   <div class="container-fluid">
+    <center>
+<input id="ad-buttonex"  lass="MyButton" type="button" value="Download Corporate Brochure" onclick="#" />
+</center>
+    
+  </div>
+
   <br>
 
 
 
 
    <!--courses-->
+
+      
+     <h3 id="ad-v-line"><span  id="ad-sub-h2">  Our Certification and Courses</h3></span>
+
+   
 <div class="container-fluid">
  <div class="row">
-      
-     <h3 id="v-line">  Our Certification and Courses</h3>
-   
-
   
   <div class="col-md-4 ">
-        <a href="#" </a><img src="images/service-management.jpg" class="img-responsive" width="260%" ></a>&nbsp
+        <a href="#"> <img src="images/service-management.jpg" class="img-fluid" width="260%"></a>
   </div>
 
        
     <div class="col-md-4">
-           <a href="#" </a><img src="images/data-analytics.jpg" class="img-responsive" width="260%"></a>&nbsp
+           <a href="#"> <img src="images/data-analytics.jpg" class="img-fluid" width="260%"></a>
    </div>
 
      <div class="col-md-4">
-             <a href="#" </a><img src="images/learning.jpg" class="img-responsive" width="260%" ></a>
+             <a href="#"> <img src="images/learning.jpg" class="img-fluid" width="260%" ></a>
      </div>
      
      </div>
@@ -213,11 +218,11 @@ Date:        <dd-mm-yyyy>
 <div class="row">
       
   <div class="col-md-4">
-       <a href="#" </a><img src="images/mobile.jpg" class="img-responsive" width="260%" ></a>&nbsp
+       <a href="#"> <img src="images/mobile.jpg" class="img-fluid" width="260%" ></a>
   </div>
 
     <div class="col-md-4">
-      <a href="#" </a><img src="images/oracle.jpg" class="img-responsive" width="260%" ></a>&nbsp
+      <a href="#"> <img src="images/oracle.jpg" class="img-fluid" width="260%" ></a>
     </div>
 
 
@@ -225,7 +230,7 @@ Date:        <dd-mm-yyyy>
           <div class="col-md-4">
              <!--   
                <a href="#" </a><img src="https://www.globaledulink.co.uk/wp-content/uploads/2017/04/CompTIA-Security-SY0-401-or-JK0-018-460x276.jpg" width="260" height="140" class="img-responsive"></a>-->
-                  <a href="#" </a><img src="images/erp.jpg" class="img-responsive" width="260%" ></a>
+                  <a href="#"> <img src="images/erp.jpg" class="img-fluid" width="260%" ></a>
         </div>
             
 </div>
@@ -235,18 +240,19 @@ Date:        <dd-mm-yyyy>
      <div class="row">
 
           <div class="col-md-4">   
-        <a href="#" </a><img src="images/cloud.jpg" class="img-responsive" width="260%" ></a>&nbsp
+        <a href="#"> <img src="images/cloud.jpg" class="img-fluid" width="260%" ></a>
      </div>
 
         <div class="col-md-4">
-       <a href="#" </a><img src="images/big-data.jpg" class="img-responsive" width="260%" ></a>&nbsp
+       <a href="#"> <img src="images/big-data.jpg" class="img-fluid" width="260%" ></a>
       </div>
 
           <div class="col-md-4">
-          <a href="#"> <img src="images/digital-marketing.jpg" class="img-responsive" width="260%" ></a>&nbsp
+          <a href="#"> <img src="images/digital-marketing.jpg" class="img-fluid" width="260%" ></a>
         </div>
    </div>
 </div>
+
 </div>
 
 
@@ -258,59 +264,53 @@ Date:        <dd-mm-yyyy>
 
     <!-- vertical bar-->
   
-    <div id="r-div" class="col-md-3" >
+    <div class="col-md-3">
       
    
-     <form >
-      <br>
-      <br>
-      <br>
-      <br>
+     <form class="ad-r-div">
+    
 
-    <h3 id="color1" align="center">Contact  Us</h3>
+    <h3 id="ad-color1" align="center">Contact  Us</h3>
   <label  for="fname"></label>
-  <input type="text" id="form-line" name="fname" placeholder="First Name &nbsp*"> 
+  <input type="text" id="ad-form-line" name="fname" placeholder="First Name &nbsp*"> 
   <label for="lname"></label>
-  <input type="text" id="form-line" name="lname" placeholder="Last Name &nbsp*">
+  <input type="text" id="ad-form-line" name="lname" placeholder="Last Name &nbsp*">
   <label for="email"></label>
-  <input type="email" id="form-line" name="email" placeholder="E-mail &nbsp *">
+  <input type="email" id="ad-form-line" name="email" placeholder="E-mail &nbsp *">
 
        <label for="phone"></label>
-       <input  type="phone" name="number" id="form-line"  placeholder="Phone Number &nbsp*">
+       <input  type="phone" name="number" id="ad-form-line"  placeholder="Phone Number &nbsp*">
        <label for="companey"></label>
-       <input type="text" name="companey" id="form-line" placeholder="Companey &nbsp *" >
+       <input type="text" name="companey" id="ad-form-line" placeholder="Companey &nbsp *" >
        <label for="counntry"></label>
-       <select  id="form-line" name="counntry">
-        <option value="india">Country &nbsp *</option>
+       <select  id="ad-form-line" name="counntry">
+        <option value="india">Country &nbsp  *</option>
           <option value="india">india</option>
           <option value="America">America</option>
           <option value="cannada">Cannada</option>
           <option value="Arub">Arub</option>
           <option value="iraq">Iraq</option>
        </select>
-       <select id="form-line" name="number of employees">
+       <select id="ad-form-line" name="number of employees">
          <option value="number"> Number of  Employees &nbsp *</option>
           <option value="number">  <  1000 employees </option>
           <option value="number">1000-5000</option>
           <option value="number">5000-10000</option>
        </select>
        <div class="checkbox" >
-       <label id="color1"><input type="checkbox" name="">I agree to be contacted over email and phone</label>
+      <label id="ad-color1"> &nbsp<input type="checkbox" name="">&nbsp&nbsp I agree to be contacted over email &nbsp &nbsp  &nbsp &nbspand phone</label>
        </div>
-      <center> <input class="btn btn-success " id="sub-button" type="submit" value="Submit"></center>
+       <br>
+      <center> <input class="btn btn-success " id="ad-sub-button" type="submit" value="Submit"></center>
      </form>
 
-    
+    </div>
    </div>
 
 
-
+</div>
+</div>
 <!-- NOTE: Please keep your code clean + readable and add comments -->
-
-
-
-
-
 
 
 
--- jobFair.php
+++ jobFair.php
@@ -23,20 +23,21 @@ Date:        18/12/17
   	<!-- Left bar starts -->
      <div class="left-bar">
       <div class="image1">
-        <img src="images/dcentmass_png.png" class="img-responsive">
+        <img src="images/dcentmass_png.png" class="img-fluid">
       </div>
-      <ul>
+     <ul>
         <!-- User class="activeLink" to make links active -->
-        <li><a href="#">Home</a></li>
-        <li><a href="#">DCent MASS</a></li>
-        <li><a href="#">Our Services</a></li>
-        <li><a href="#">Student Hub</a></li>
-        <li><a href="#">Courses</a></li>
-        <li><a href="#">Events</a></li>
-        <li><a href="#">Media</a></li>
-        <li><a href="#">Forum</a></li>
-        <li><a href="#">Contact Us</a></li>
-        <li style="border-bottom:  2px solid #06f;"><a href="#">FAQs</a></li>
+        <li class="activeLink"><a href="#"><i class="material-icons">home</i><span class="menuText">Home</span></a></li>
+        <li><a href="#"><i class="material-icons">language</i><span class="menuText">DCent MASS</span></a></li>
+        <li><a href="#"><i class="material-icons">settings_applications</i><span class="menuText">Our Services</span></a></li>
+        <li><a href="#"><i class="material-icons">group_work</i><span class="menuText">Student Hub</span></a></li>
+        <li><a href="#"><i class="material-icons">library_books</i><span class="menuText">Courses</span></a></li>
+        <li><a href="#"><i class="material-icons">event</i><span class="menuText">Events</span></a></li>
+        <li><a href="#"><i class="material-icons">perm_media</i><span class="menuText">Media</span></a></li>
+        <!-- <li><a href="#">Forum</a></li> -->
+        <li><a href="#"><i class="material-icons">contacts</i><span class="menuText">Contact Us</span></a></li>
+        <!-- <li style="border-bottom:  2px solid #06f;"><a href="#"></a></li> -->
+        <!-- <li style="border-bottom: 2px solid #06f;"><a href="#">FAQs</a></li> -->
       </ul>
      </div>
      <!-- Left bar ends -->
@@ -99,7 +100,7 @@ Date:        18/12/17
    
      <div  class="row">
     
-       <div class="col-md-9 " id="b-img" style="min-height: 780px;" >
+       <div class="col-md-9 " id="ad-backgroundimg" style="min-height: 780px;" >
       
     
  
@@ -107,42 +108,42 @@ Date:        18/12/17
     <br>
     <br>
     <br>
-  <div class="container">
-        <h1 id="h-font"> WE WANT TO</h1>
+  <div class="ad-container">
+        <h1 id="ad-h-font"> WE WANT TO</h1>
    
-   <h1 id="h-font">GIVE YOU</h1>
-   <hr>
+   <h1 id="ad-h-font">GIVE YOU</h1>
+   <hr id="ad-hrline">
    
 
    
-   <h1 id="h-font"> A JOB!</h1>
-    <h5 style="font-size: 13px; font-weight: bold;" id="icon-Paragraph" >&nbspIMMEDIATE HIRING</h5>
+   <h1 id="ad-h-font"> A JOB!</h1>
+    <h5 style="font-size: 13px; font-weight: bold;">&nbspIMMEDIATE HIRING</h5>
     <br>
     
-    <h3 id="h-font"> Email Us :<a href="#">hiring@dcentmass.com</a></h3>  
+    <h3 id="ad-P-heading"> Email Us :<a href="#">hiring@dcentmass.com</a></h3>  
 
       <br>
       <br>
-   <hr>
-    <h4 id="P-heading2">Address: No.158, Near</h4>
-    <h4 id="P-heading2"> Ashoka gardan.,
+   <hr id="ad-hrline">
+    <h4 id="ad-P-heading2">Address: No.158, Near</h4>
+    <h4 id="ad-P-heading2"> Ashoka gardan.,
      Dharwad Rd,</h4>
-    <h4 id="P-heading2"> Haliyal, Karnataka 581329</h4>                   
-     <h4 id="P-heading2">Open: 24 hours</h4>
-    <h4 id="P-heading2">Phone: 084958 13777 </h4>
+    <h4 id="ad-P-heading2"> Haliyal, Karnataka 581329</h4>                   
+     <h4 id="ad-P-heading2">Open: 24 hours</h4>
+    <h4 id="ad-P-heading2">Phone: 084958 13777 </h4>
        
        </div>
       
    </div>
 
-   <div class="col-md-3  bg-primary" style="min-height:780px;">
+   <div class="col-md-3 " style="min-height:780px;background-color: #0e5e96;">
     <br>
     <br>
     <br>
     <br>
     
-   <center font-weight="bold">  <img class=" img-responsive" src="images/winnew.png" ></center>
-  <center id=" P-heading"> <h3>  New begining.</h3>
+   <center font-weight="bold">  <img class=" img-fluid" src="images/winnew.png" ></center>
+  <center id=" ad-P-heading"> <h3>  New begining.</h3>
 <h3 >New mindest.</h3>
 <h3>New focus.</h3>
 <h3>New start.</h3>
